79b0cae288df4ae4a90bf9c258a7c566e274fc9e
abhishekgarfield <abhishek2759@gmail.com>
2022-07-12 21:52:34 +0530

    phon nav

--- zomato.js
+++ zomato.js
@@ -29,9 +29,28 @@
                 {
                     el.style.display="block";
                 }
-                else
+                elsel
                 {
                     el.style.display="none";
+                }
+            }
+            function myphone(x)
+            {
+                var el=document.getElementById("nav_phon_cont");
+                var th =document.getElementById("phon_nav");
+                
+                if(el.style.display ==="none")
+                {
+                    el.style.display="block";
+                    th.style.backgroundColor="white"
+                    th.style.width="100%";
+                    x.className="fa fa-remove";
+                }
+                else
+                {
+                    el.style.display="none"; 
+                    th.style.backgroundColor="transparent";
+                    x.className="fa fa-bars";
                 }
             }
             
