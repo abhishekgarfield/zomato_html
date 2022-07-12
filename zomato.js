d5a70fab0d1b185e2712d8949592c859acb9226b
abhishekgarfield <abhishek2759@gmail.com>
2022-07-12 14:24:53 +0530

    js_added_externally

---
+++ zomato.js
@@ -0,0 +1,37 @@
+            function myfunc()
+            {
+                var el=document.getElementById("option_1");
+                if(el.style.display==="none")
+                {
+                    el.style.display="block";
+                }
+                else
+                {
+                    el.style.display="none";
+                }
+            }
+            function myfunct()
+            {
+                var el=document.getElementById("option_2");
+                if(el.style.display==="none")
+                {
+                    el.style.display="block";
+                }
+                else
+                {
+                    el.style.display="none";
+                }
+            }
+            function myfunctio()
+            {
+                var el=document.getElementById("option_3");
+                if(el.style.display==="none")
+                {
+                    el.style.display="block";
+                }
+                else
+                {
+                    el.style.display="none";
+                }
+            }
+            
