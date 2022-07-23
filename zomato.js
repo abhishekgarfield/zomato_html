deaff00f9255248ee48f2620a35ac57b1ec1bcac
abhishekgarfield <abhishek2759@gmail.com>
2022-07-23 09:37:32 +0530

    js error fixed

--- zomato.js
+++ zomato.js
@@ -29,7 +29,7 @@
                 {
                     el.style.display="block";
                 }
-                elsel
+                else
                 {
                     el.style.display="none";
                 }
